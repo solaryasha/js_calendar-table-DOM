d31ca5798a6a7b75bf7769b04bb0b9728508e738
solaryasha <solaryasha@gmail.com>
2020-06-14 23:02:39 +0300

    My first naive solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,62 @@
 const calendar = document.querySelector('#calendar');
 
 function calendarTable(year, month, element) {
-  // WRITE YOUR CODE HERE
+  const amountOfDays = new Date(year, month, 0).getDate();
+  const startDay = new Date(`${year}-${month}-01`).getDay() - 1;
+
+  const table = document.createElement('table');
+
+  table.append(createHeader());
+  table.append(createBody(amountOfDays, startDay));
+
+  element.append(table);
 }
 
-calendarTable(2019, 10, calendar);
+const createHeader = () => {
+  const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+  const thead = document.createElement('thead');
+
+  days.forEach(day => {
+    const th = document.createElement('th');
+
+    th.textContent = day;
+    thead.append(th);
+  });
+
+  return thead;
+};
+
+const createBody = (day, start) => {
+  const tbody = document.createElement('tbody');
+  let canPopulate = false;
+  let counter = start;
+  const weeks = Math.ceil((day + start) / 7);
+
+  for (let i = 0; i < weeks; i++) {
+    const row = document.createElement('tr');
+
+    tbody.append(row);
+
+    for (let j = 0; j < 7; j++) {
+      const td = document.createElement('td');
+
+      if (j === counter) {
+        canPopulate = true;
+      }
+
+      if (canPopulate) {
+        td.innerText = counter - start + 1;
+        counter++;
+      }
+
+      if (counter - start === day) {
+        canPopulate = false;
+      }
+      row.append(td);
+    }
+  }
+
+  return tbody;
+};
+
+calendarTable(2020, 1, calendar);
